2ccbc8140f358c49b98ad71610d005ee843991ca
Julian Silvestri <juliansilvestri92@gmail.com>
2025-04-01 20:13:55 -0400

    Initial Commit

---
+++ test_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_app
+//
+//  Created by Julian Silvestri on 2025-04-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ test_appTests/test_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  test_appTests.swift
+//  test_appTests
+//
+//  Created by Julian Silvestri on 2025-04-01.
+//
+
+import Testing
+@testable import test_app
+
+struct test_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
